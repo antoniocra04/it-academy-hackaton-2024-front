fb901563acce8af1c7edd3fbcb3162f6837343f5
Anton Ryazanov <antoniocra04@gmail.com>
2024-05-21 17:26:00 +0700

    feature: add router-dom and vite config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,39 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [ 'plugin:react/recommended', 'plugin:prettier/recommended'],
+	parser: "@typescript-eslint/parser",
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-uses-react': 'off',
+		'react/prop-types': 'off',
+		'@typescript-eslint/strict-boolean-expressions': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-floating-promises': 'off',
+		'@typescript-eslint/return-await': 'off',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts'],
+};
