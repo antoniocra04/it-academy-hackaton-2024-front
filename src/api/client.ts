3744a90ebb3be27be2570e72c0340706ad24ba0d
Anton Ryazanov <antoniocra04@gmail.com>
2024-05-24 19:25:54 +0700

    fix: change api url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BACKEND_URL = 'https://backend:8080';
+export const BACKEND_URL = 'http://localhost:8080';
 
 export const axiosClient = axios.create({
 	baseURL: BACKEND_URL,
