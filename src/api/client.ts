92e724e48807626382b3d52dd7a353e043e0f5d1
inittose <pol.konnik@gmail.com>
2024-05-26 19:27:09 +0700

    fix(client.ts): Поменял url бекенда

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { store } from '../store/index';
 
-export const BACKEND_URL = 'http://localhost:8080/api';
+export const BACKEND_URL = 'http://147.45.158.95:8080/api';
 
 export const axiosClient = axios.create({
 	baseURL: BACKEND_URL,
